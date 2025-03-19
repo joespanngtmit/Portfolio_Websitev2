7f42a0a28d81f2d3639adc0f8962ca50668a0477
Rohan Goyal <rohangoyal264@gmail.com>
2025-03-19 20:20:48 +0530

    Refactor ThemeContext to simplify theme toggling logic and improve localStorage handling

--- src/components/ParticleBackground/ThemeContext.js
+++ src/components/ParticleBackground/ThemeContext.js
@@ -4,27 +4,19 @@ export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [isDarkTheme, setIsDarkTheme] = useState(() => {
-    // Get the saved theme from localStorage or default to dark theme
-    const savedTheme = localStorage.getItem("theme");
-    return savedTheme === "dark" ? true : false;
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme === 'dark' ? true : true; // Default to dark theme
   });
 
-  // Toggle theme and apply the appropriate class to the body
   const toggleTheme = () => {
     const newTheme = !isDarkTheme;
     setIsDarkTheme(newTheme);
-    localStorage.setItem("theme", newTheme ? "dark" : "light"); // Save the new theme in localStorage
+    localStorage.setItem('theme', newTheme ? 'dark' : 'light');
   };
 
   useEffect(() => {
-    // Apply the appropriate class to the body when the theme is toggled
-    if (isDarkTheme) {
-      document.body.classList.add("dark-theme");
-      document.body.classList.remove("light-theme");
-    } else {
-      document.body.classList.add("light-theme");
-      document.body.classList.remove("dark-theme");
-    }
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+    document.body.classList.toggle('light-theme', !isDarkTheme);
   }, [isDarkTheme]);
 
   return (
